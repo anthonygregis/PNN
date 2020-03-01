99646df140e5e5eb319ba0987432df406cca3bc2
thedevtron <anthony@thedevtron.com>
2020-02-29 22:34:18 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+$conn = mysqli_connect("localhost", "pdadmin", "v9Tc^n89", "PDMLeak");
+
+$affectedRow = 0;
+
+$xml = simplexml_load_file("input.xml") or die("Error: Cannot create object");
+
+foreach ($xml->children() as $row) {
+    $name = $row->Name;
+    $phone = $row->Citizen;
+    $licensePlate = $row->LicensePlate;
+    $vehicle = $row->Vehicle;
+    
+    $sql = "INSERT INTO transactions(name,phone,licensePlate,vehicle) VALUES ('" . $name . "','" . $phone . "','" . $licensePlate . "','" . $vehicle . "')";
+    
+    $result = mysqli_query($conn, $sql);
+    
+    if (! empty($result)) {
+        $affectedRow ++;
+    } else {
+        $error_message = mysqli_error($conn) . "\n";
+    }
+}
+?>
+<h2>Insert XML Data to MySql Table Output</h2>
+<?php
+if ($affectedRow > 0) {
+    $message = $affectedRow . " records inserted";
+} else {
+    $message = "No records inserted";
+}
+
+?>
