ecc4a93c1eb0694c85b8a6e6cbdc2ce68c0ecb0d
thedevtron <anthony@thedevtron.com>
2020-03-01 01:20:16 -0500

    PHP Conversion

--- index.php
+++ index.php
@@ -1,35 +1,58 @@
 <?php
-$conn = mysqli_connect("localhost", "pdmadmin", "v9Tc^n89", "PDMLeak");
+session_start();
 
-$affectedRow = 0;
-
-$xml = simplexml_load_file("input.xml") or die("Error: Cannot create object");
-
-foreach ($xml->children() as $row) {
-    $name = $row->Name;
-    $citizenID = $row->CitizenID;
-    $phone = $row->Phone;
-    $licensePlate = $row->LicensePlate;
-    $vehicle = $row->Vehicle;
-    
-    $sql = "INSERT INTO transactions(name,citizenID,phone,licensePlate,vehicle) VALUES ('" . $name . "','" . $citizenID . "','" . $phone . "','" . $licensePlate . "','" . $vehicle . "')";
-    
-    $result = mysqli_query($conn, $sql);
-    
-    if (! empty($result)) {
-        $affectedRow ++;
-    } else {
-        $error_message = mysqli_error($conn) . "\n";
-    }
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $category = trim($_POST["category"]);
+    $search = trim($_POST["search"]);
+    header("location: search-result.php?" . $category . "=" . $search);
 }
 ?>
-<h2>Insert XML Data to MySql Table Output</h2>
-<?php
-if ($affectedRow > 0) {
-    $message = $affectedRow . " records inserted";
-} else {
-    $message = "No records inserted";
-}
-    echo $message;
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="author" content="colorlib.com">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
+    <link href="css/main.css" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="s003">
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" >
+        <div class="inner-form">
+          <div class="input-field first-wrap">
+            <div class="input-select">
+              <select data-trigger="" name="category" id="category" value="<?php echo $category; ?>">
+                <option placeholder="">Category</option>
+                <option>Name</option>
+                <option>Citizen ID</option>
+                <option>Phone</option>
+                <option>License Plate</option>
+                <option>Vehicle</option>
+              </select>
+            </div>
+          </div>
+          <div class="input-field second-wrap">
+            <input id="search" type="text" placeholder="Enter Info" name="search" id="search" value="<?php echo $search; ?>"/>
+          </div>
+          <div class="input-field third-wrap">
+            <button class="btn-search" type="button">
+              <svg class="svg-inline--fa fa-search fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+    <script src="js/extention/choices.js"></script>
+    <script>
+      const choices = new Choices('[data-trigger]',
+      {
+        searchEnabled: false,
+        itemSelectText: '',
+      });
 
-?>
+    </script>
+  </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</html>
