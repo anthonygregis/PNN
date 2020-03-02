2596d9822f4792ad2c31d488b508c2c3cbf8e08a
thedevtron <anthony@thedevtron.com>
2020-03-01 21:24:48 -0500

    Connect DB

--- delete.php
+++ delete.php
@@ -1,4 +1,14 @@
 <?php 
+// Include config file
+require_once "assets/action/config.php";
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 
 if(isset($_REQUEST['id']) && is_numeric($_REQUEST['id']))
 {
