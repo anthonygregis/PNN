6e595b9aa498eaf972aa898a4bde68f8a01a3399
thedevtron <anthony@thedevtron.com>
2020-03-03 19:35:24 -0500

    Debug

--- search-result.php
+++ search-result.php
@@ -20,13 +20,7 @@ if(isset($_GET["cat"]) && isset($_GET["search"])){
 	//Updated Here
 	$result = $query -> execute();
 
-	if ($result->num_rows >= 1)
-	{
-	    $assocData = $result->fetch_array(MYSQLI_ASSOC);
-	    echo $assocData;
-	}  else {
-		echo "no results";
-	}
+	echo $result;
 
 
 //Close Connection
